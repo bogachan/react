1e061ffb1d048cbf58372027042a382919cf57ea
bogachan <bgchn@yandex.com>
2020-04-20 16:18:28 +0300

    Başlangıç

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewsItem from './NewsItem';
+
+function App() {
+  return (
+    <div className="App">
+      <p>MERHABA AQ --- SEnin içinden geçip avradını sikicem ...</p>
+      <NewsItem></NewsItem>
+      <NewsItem></NewsItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NewsItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewsItem extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>MERHABA REACTTT</h1>
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
